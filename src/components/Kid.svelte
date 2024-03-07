f2cc223bd4e1214960fa008bfaefef453acc9fbb
frizzy139 <paolofri20@gmail.com>
2024-03-07 17:21:19 +0100

    nuovo

---
+++ src/components/Kid.svelte
@@ -0,0 +1,61 @@
+<div class="card">
+	<div>
+		<h1 class="titolo">Kid Yugi</h1>
+	</div>
+	<div class="descrizione">
+		<p>
+			Kid Yugi è un artista italiano originario di Massafra, Puglia. Il suo genere musicale spazia
+			tra l'hip-hop, il rap e la trap, caratterizzato da testi intimi e ritmi avvincenti.
+		</p>
+	</div>
+	<div class="info">
+		<div>
+			<p>Location</p>
+			<p>Palco 1</p>
+		</div>
+		<div>
+			<p>Show</p>
+			<p>Mercoledi 23 Aprile</p>
+		</div>
+		<div>
+			<p>Find Artist</p>
+			<p>Instagram</p>
+			<p>Spotify</p>
+		</div>
+	</div>
+</div>
+
+<style>
+	.card {
+		height: 100vh;
+		background-image: url('https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTdJUd2rckciDzjSD1KGeDqK36A7DdM0RugjySvjebvRf_AJnaJ');
+		background-size: cover;
+		background-position: center;
+		color: yellow;
+	}
+
+	.montserrat-normal {
+		font-family: 'Montserrat', sans-serif;
+		font-optical-sizing: auto;
+		font-weight: normal;
+		font-style: normal;
+	}
+
+	.info {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		padding: 30px;
+	}
+
+	.titolo {
+		text-align: center;
+		font-size: 70px;
+		font-weight: 700;
+	}
+
+	.descrizione {
+		font-size: 20px;
+		padding: 30px;
+	}
+</style>
